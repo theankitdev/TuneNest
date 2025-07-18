5cacb67421f9a73165ab5a752c11529932098c5a
Ankit <kumarankit783821@gmail.com>
2025-07-18 16:02:30 +0530

    added

--- app/(detailedViews)/artistPage.jsx
+++ app/(detailedViews)/artistPage.jsx
@@ -138,7 +138,7 @@ const ArtistPage = () => {
     order_id: '',
     prefill: {
       email: user.email ,
-      contact: '9191919191',
+      contact: '7982196893',
       name: `${user.firstName} ${user.lastName}`,
     },
     theme: {color: '#53a20e'}
