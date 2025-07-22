39c25e3032923236f6571edacfa215b3f6bed5e8
Ankit <kumarankit783821@gmail.com>
2025-07-22 23:41:46 +0530

    fb login added

--- src/config/firebase/FacebookSignIn.js
+++ src/config/firebase/FacebookSignIn.js
@@ -3,7 +3,7 @@ import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
 
 export async function onFacebookButtonPress() {
   // Attempt login with permissions
-  const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+  const result = await LoginManager.logInWithPermissions(['public_profile']);
 
   if (result.isCancelled) {
     throw 'User cancelled the login process';
